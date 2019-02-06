04245104ad3fcf7ff9c6e7bc28ca97ff4e72ca0d
Steven Nguyen <37124470+snguyen01@users.noreply.github.com>
2019-02-06 10:17:53 -0800

    MySwingApplication

--- Projects/MySwingApplication/CanvasComponent.java
+++ Projects/MySwingApplication/CanvasComponent.java
@@ -1,28 +1,87 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 /**
  * Project 1 - MySwingApplication
  *
  * @ Steven Nguyen
  * @ 2/4/19
  */
-
-public class CanvasComponent extends JComponent {
-    int xPos;
-    int yPos;
-    int width;
-    int height;
-    
-    public CanvasComponent(int width, int height){
-        setSize(width, height);
-        xPos = 200;
-        yPos = 200;
-        width = 400;
-        height = 400;
+public class CanvasComponent extends JComponent implements MouseListener, MouseMotionListener, ActionListener
+{
+    int x = 100;
+    int y = 100;
+    int width = 100;
+    int height = 100;
+    int mouseFromX;
+    int mouseFromY;
+    int mouseToX;
+    int mouseToY;
+    boolean shapeSelected;
+    int animationDeltaX = 1;
+    int animationDeltaY = 0;
+    int gutterX = 10;
+    int gutterY = 10;
+    Dimension componentSizeDimension;
+    public CanvasComponent(int height, int width)
+    {
+        setSize(height, width);
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+    }               
+    public void paintComponent(Graphics g)
+    {
+        g.setColor(Color.red);
+        g.fillRect(x, y, width, height);
     }
+    //MouseListener
+    public void mouseClicked(MouseEvent e)
+    {
     
-    public void paintComponent(int xPos, int yPos, int width, int height){
-        //setColor(Color.red);
+    }
+    public void mousePressed(MouseEvent e)
+    {
+        mouseFromX = e.getX();
+        mouseFromY = e.getY();
+        if((mouseFromX >= x && mouseFromX <= x + height) && (mouseFromY >= y && mouseFromY <= y + width))
+        {
+            shapeSelected = true;
+        }
+        else
+        {
+            shapeSelected = false;
+        }
+    }
+    public void mouseReleased(MouseEvent e)
+    {
+    
+    }
+    public void mouseEntered(MouseEvent e)
+    {
+    
+    }
+    public void mouseExited(MouseEvent e)
+    {
+    
+    }
+    //MouseMotionListener
+    public void mouseDragged(MouseEvent e)
+    {
+        mouseToX = e.getX();
+        mouseToY = e.getY();
+        if(shapeSelected)
+        {
+            x += mouseToX - x;
+            y += mouseToY - y;
+            repaint();
+        }
+    }
+    public void mouseMoved(MouseEvent e)
+    {
         
     }
-}
+    public void actionPerformed(ActionEvent e)
+    {
+        componentSizeDimension = getSize();
+    }
+}    
--- Projects/MySwingApplication/MySwingApplication.java
+++ Projects/MySwingApplication/MySwingApplication.java
@@ -5,17 +5,23 @@ import javax.swing.*;
  * @author (Steven Nguyen)
  * @version (2419)
  */
-public class MySwingApplication implements Runnable {
-    JFrame jFrame;
-    public void run() {
-        jFrame = new JFrame("Window");
-        jFrame.setSize(400, 400);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setVisible(true);
+import javax.swing.*;
+import java.awt.event.*;
+
+public class MySwingApplication implements Runnable
+{
+    CanvasComponent paint = new CanvasComponent(100, 100);
+    public void run()
+    {
+        JFrame jframe = new JFrame("Window");
+        jframe.setSize(4000, 4000);       
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(paint);
+        jframe.setVisible(true);
     }
-    
-    public static void main() {
-       MySwingApplication mySwingApplicationSwing = new MySwingApplication();
-       javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
+    public static void main(String[] args)
+    {
+        MySwingApplication mySwingApplicationSwing = new MySwingApplication();
+        javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
     }
 }
